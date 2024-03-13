6706b4233888b2c63d76b048d3b30dcbf8b203bb
Edib Esad Galip <edibesad@gmail.com>
2024-03-13 11:56:21 +0300

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by Edib Esad Galip on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
